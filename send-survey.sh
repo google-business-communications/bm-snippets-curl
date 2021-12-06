c40dbedc30130fd4cff2a81bf9f1ef3eb506c8d6
Felipe Hlibco <felipehlibco@google.com>
2021-12-06 13:03:36 -0800

    Remove all files
    Change-Id: I3949a50a99385ff3c70856dfd32c768f35909104

--- send-survey.sh
+++
@@ -1,7 +0,0 @@
-# Replace the __CONVERSATION_ID__
-# Make sure a service account key file exists at ./service_account_key.json
-
-curl -X POST "https://businessmessages.googleapis.com/v1/conversations/__CONVERSATION_ID__/surveys?surveyId=f4bd7576-6c2e-4674-9db4-d697166f63ce" \
--H "Content-Type: application/json" \
--H "User-Agent: curl/business-messages" \
--H "`oauth2l header --json ./service_account_key.json businessmessages`" \
